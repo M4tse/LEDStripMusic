5e310c9151abd01387a8918a7257b62bb7692248
M4tse <mm903-1@bingo-ev.de>
2023-05-26 19:39:33 +0200

    :q

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,38 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266Audio.h>
+#include "AudioGenerator.h"
+#include "AudioOutput.h";
+const char* ssid = "bytewerk";
+const char* password = "bluemchenwiese";
+
+const char* radioUrl = "https://www.radio.net/s/jazz24";
+AudioFileSourceHTTPStream *file = new AudioFileSourceHTTPStream(radioUrl);
+AudioGenerator generator;
+// Set up the audio output
+AudioOutputI2S audioOutput;
+AudioFileSourceICYStream audioStream;
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi!");
+
+    audioOutput.begin();
+    
+    audioStream.open(radioUrl);
+    
+    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    if (audioStream.isOpen()) {
+        //const uint16_t* waveforms = audioStream.
+        //generator.begin(file, );
+    }
 }
